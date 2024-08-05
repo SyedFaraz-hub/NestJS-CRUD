41fbe122fde137f1b252117a05aa71127844570a
espark-farazshah <farazshah@esparkconsultants.com>
2024-08-05 13:27:16 +0500

    employee CRUD with noeon postgre database completed

---
+++ src/all-exceptions.filter.ts
@@ -0,0 +1,50 @@
+import { Catch, ArgumentsHost, HttpStatus, HttpException } from "@nestjs/common";
+import { BaseExceptionFilter } from "@nestjs/core";
+import { Request, Response } from "express";
+import { MyLoggerService } from "./my-logger/my-logger.service";
+import { PrismaClientValidationError } from "@prisma/client/runtime/library";
+
+type MyResponseObj = {
+    statusCode: number,
+    timeStamp: string,
+    path: string,
+    response: string | object
+}
+
+@Catch()
+export class AllExceptionsFiltes extends BaseExceptionFilter {
+    private readonly logger = new MyLoggerService(AllExceptionsFiltes.name)
+
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+
+        const myResponseObj: MyResponseObj = {
+            statusCode: 200,
+            timeStamp: new Date().toDateString(),
+            path: request.url,
+            response: ''
+        }
+
+    if (exception instanceof HttpException) {
+        myResponseObj.statusCode = exception.getStatus()
+        myResponseObj.response = exception.getResponse()
+    }    
+
+    else if (exception instanceof PrismaClientValidationError) {
+        myResponseObj.statusCode = 422;
+        myResponseObj.response = exception.message.replaceAll(/\n/g, '')
+    }
+    else {
+        myResponseObj.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+        myResponseObj.response = 'Internal Server Error'
+    }
+
+    response.status(myResponseObj.statusCode).json(myResponseObj)
+    this.logger.error(myResponseObj.response, AllExceptionsFiltes.name)
+
+    super.catch(exception, host)
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFiltes } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const {httpAdapter} = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExceptionsFiltes(httpAdapter))
+
+  app.enableCors()
+  app.setGlobalPrefix('api')
   await app.listen(3000);
 }
 bootstrap();
